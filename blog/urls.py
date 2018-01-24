5621d2e587d0bc94816fb9d40c1acd4b47ecfb17
cinthia <cinthiajazross@gmail.com>
2018-01-24 10:55:37 -0600

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 08:48:49 2018
+
+@author: kreamas
+"""
+
+from django.conf.urls import url
+from.import views
+
+urlpatterns=[
+    url(r'^$',views.post_list,name='post_list'),            
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+from _future_ import unicode_literals
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request,'blog/post_list.html',{})
+    
